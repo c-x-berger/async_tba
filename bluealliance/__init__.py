4f0a0102652800c91e68e0d6b5ac9f57f5dd6e22
Caleb Berger <caleb.x.berger@gmail.com>
2018-09-25 13:30:25 -0400

    Add imports for Team, mini models in __init__

--- bluealliance/__init__.py
+++ bluealliance/__init__.py
@@ -3,4 +3,6 @@ __author__ = 'Caleb Xavier Berger'
 __copyright__ = '(c) 2018 CXB'
 
 from . import constants
+from .team import Team
 from .bluealliance import Blualliance
+from .mini_models import *
