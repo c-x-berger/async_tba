7ba96c90afad1e1264ab798a2d18337294cd5d88
Caleb Berger <caleb.x.berger@gmail.com>
2018-10-01 15:57:28 -0400

    Add SimpleEvent

--- bluealliance/event.py
+++ bluealliance/event.py
@@ -7,7 +7,30 @@ from .team import Team
 from .mini_models import Datacache
 
 
-class Event(Model):
+class SimpleEvent(Model):
+    def __init__(self, conn_state: ConnectionState, key: str = None, name: str = None, event_code: str = None,
+                 city: str = None, state_prov: str = None, country: str = None,
+                 start_date: str = None, end_date: str = None, year: int = None):
+        super().__init__(conn_state, key=key)
+
+        self.name = name
+        self.event_code = event_code
+
+        self.city = city
+        self.state_prov = state_prov
+        self.country = country
+
+        self.start_date, self.end_date = start_date, end_date
+        self.year = year
+
+    async def get_alliances(self):
+        return [Alliance(self._session, **a) for a in await self.connection.get_alliances(self.key)]
+
+    async def get_teams(self):
+        return [Team(self._connection, **s) for s in await self.connection.get_event_teams(self.key)]
+
+
+class Event(SimpleEvent):
     def __init__(self, conn_state: ConnectionState, key: str = None, name: str = None, event_code: str = None,
                  event_type: int = None, district: dict = None, city: str = None,
                  state_prov: str = None, country: str = None, start_date: str = None,
@@ -18,14 +41,13 @@ class Event(Model):
                  website: str = None, first_event_id: str = None, first_event_code: str = None,
                  webcasts: list = None, division_keys: list = None, parent_event_key: str = None,
                  playoff_type: int = 0, playoff_type_string: str = None):
-        super().__init__(conn_state, key=key)
+        super().__init__(conn_state, key=key, name=name, event_code=event_code, city=city,
+                         state_prov=state_prov, country=country, start_date=start_date, end_date=end_date, year=year)
 
-        self.name, self.short_name = name, short_name
-        self.event_code = event_code
+        self.short_name = name, short_name
         self.event_type = event_type
         self.district = district
-        self.city, self.state_prov, self.country = city, state_prov, country
-        self.start_date, self.end_date, self.year, self.week = start_date, end_date, year, week
+        self.week = week
         self.address, self.postal_code = address, postal_code
         self.gmaps_place_id, self.gmaps_url = gmaps_place_id, gmaps_url
         self.lat, self.lng = lat, lng
@@ -36,9 +58,3 @@ class Event(Model):
         self.webcasts = webcasts
         self.division_keys, self.parent_event_key = division_keys, parent_event_key
         self.playoff_type, self.playoff_type_string = playoff_type, playoff_type_string
-
-    async def get_alliances(self):
-        return [Alliance(self._session, **a) for a in await self.connection.get_alliances(self.key)]
-
-    async def get_teams(self):
-        return [Team(self._connection, **s) for s in await self.connection.get_event_teams(self.key)]
